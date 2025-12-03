099490dfac49a81c9399fea4319d4f8d1a83b7f0
Kelson Batista <93272521+KelsonB@users.noreply.github.com>
2025-12-03 16:17:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -661,6 +661,6 @@ def responder_proposta():
 
 
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
-
+    # init_db()  # COMENTE esta linha
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
